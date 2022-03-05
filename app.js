deaf222784e166a97e9430c933ad47ab36f22cd6
Trung Bui <buitrung.dev@gmail.com>
2022-03-05 23:00:00 +0700

    Implement exchange site

--- app.js
+++ app.js
@@ -1,14 +1,143 @@
+const axios = require('axios');
 const express = require('express');
-// const socketio = require('socket.io');
+const socketio = require('socket.io');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
-const server = app.listen(process.env.PORT || 3000, () => {
+const order_cnt = 10;
+
+function generateNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function generateSizes(max_total_size) {
+  let rand_arr = [];
+  for (let i = 0; i < order_cnt; i++) {
+    rand_arr.push(Math.random());
+  }
+
+  let rand_total = rand_arr.reduce((sum, num) => sum + num, 0);
+  let generated_sizes = rand_arr.map(rand_num => rand_num * max_total_size / rand_total);
+
+  return generated_sizes;
+}
+
+function generateOrders(order_type, original_price, original_size) {
+  if (order_type != 'bid' && order_type != 'ask') {
+    throw new Error(`Unknown order_type: ${order_type}`);
+  }
+
+  const max_bid_volume = generateNumber(4, 5);
+  const max_ask_size = generateNumber(140, 150);
+
+  let decimal_cnt = 0;
+  if (Math.floor(original_price) != original_price) {
+    decimal_cnt = original_price.toString().split('.')[1].length || 0;
+  }
+
+  let is_price_desc = (order_type == 'bid') ? true : false;
+  let max_total_size = (order_type == 'bid')
+    ? max_bid_volume / original_price
+    : max_ask_size;
+
+  let generated_sizes = generateSizes(max_total_size);
+
+  let generated_orders = [
+    {
+      price: original_price,
+      size: generated_sizes[0]
+    }
+  ];
+  for (let i = 1; i < order_cnt; i++) {
+    let generated_price = is_price_desc
+      ? original_price - i * Math.pow(10, -decimal_cnt)
+      : original_price + i * Math.pow(10, -decimal_cnt);
+    generated_price = parseFloat(generated_price).toFixed(decimal_cnt);
+
+    generated_orders.push({
+      price: generated_price,
+      size: generated_sizes[i]
+    });
+  }
+
+  return generated_orders;
+}
+
+async function getOrders() {
+  const symbols = 'tETHBTC';
+
+  let symbol_res = await axios.get(`https://api-pub.bitfinex.com/v2/tickers?symbols=${symbols}`)
+
+  let [
+    symbol,
+    bid, bid_size,
+    ask, ask_size,
+    daily_change, daily_change_relative,
+    last_price,
+    volume,
+    high, low
+  ] = symbol_res.data[0];
+
+  // console.log({
+  //   symbol,
+  //   bid, bid_size,
+  //   ask, ask_size,
+  //   daily_change, daily_change_relative,
+  //   last_price,
+  //   volume,
+  //   high, low
+  // });
+
+  let all_bid_orders = generateOrders('bid', bid, bid_size);
+  let all_ask_orders = generateOrders('ask', ask, ask_size);
+
+  let orders = {
+    bid: all_bid_orders,
+    ask: all_ask_orders
+  };
+  return orders;
+}
+
+app.get('/api/orders', async (req, res) => {
+  let orders = await getOrders();
+  res.json(orders);
+});
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
   console.log("server is running");
+});
+
+const domain = (process.env.NODE_ENV == 'production')
+  ? 'http://exchange-site22.herokuapp.com'
+  : `http://localhost:${port}`;
+
+console.log('process.env.NODE_ENV=', process.env.NODE_ENV);
+console.log('domain=', domain);
+
+const io = socketio(server, {
+  cors: {
+    origin: domain,
+    methods: ['GET', 'POST'],
+    transports: ['websocket', 'polling'],
+    credentials: true
+  },
+  allowEIO3: true
+});
+
+io.on('connection', (socket) => {
+  console.log("New user connected")
+
+  setInterval(async () => {
+    let orders = await getOrders();
+    io.sockets.emit('fetch_orders', orders);
+  }, 3000);
 });
---
+++ public/js/index.js
@@ -0,0 +1,63 @@
+function getOrders(callback) {
+  let xmlhttp = new XMLHttpRequest();
+  xmlhttp.open('GET', '/api/orders', true);
+  xmlhttp.responseType = 'json';
+  xmlhttp.onload = function() {
+    let data = xmlhttp.response;
+    callback(data);
+  }
+  xmlhttp.send();
+}
+
+function renderOrders(orders) {
+  let bid_orders_comp = document.querySelector('#bid-orders');
+  let ask_orders_comp = document.querySelector('#ask-orders');
+  let total_bid_volume_comp = document.querySelector('#total-bid-volume');
+  let total_ask_size_comp = document.querySelector('#total-ask-size');
+
+  let {
+    bid: bid_orders,
+    ask: ask_orders
+  } = orders;
+
+  let bid_orders_content = '';
+  let ask_orders_content = '';
+
+  for (let bid_order of bid_orders) {
+    bid_orders_content += `
+      <tr>
+        <td>${bid_order.size}</td>
+        <td class="text-right">${bid_order.price}</td>
+      </tr>
+    `;
+  }
+  for (let ask_order of ask_orders) {
+    ask_orders_content += `
+      <tr>
+        <td>${ask_order.price}</td>
+        <td class="text-right">${ask_order.size}</td>
+      </tr>
+    `;
+  }
+
+  bid_orders_comp.innerHTML = bid_orders_content;
+  ask_orders_comp.innerHTML = ask_orders_content;
+
+  total_bid_volume_comp.innerHTML = bid_orders
+    .reduce((sum, order) => sum + order.price * order.size, 0);
+  total_ask_size_comp.innerHTML = ask_orders
+    .reduce((sum, order) => sum + order.size, 0);
+}
+
+(function() {
+  let socket = io.connect(window.location.origin);
+
+  socket.on('fetch_orders', orders => {
+    renderOrders(orders);
+  });
+
+  // Since we use socket.io to fetch orders, no need to call API by Ajax when start
+  // getOrders(function(orders) {
+  //   renderOrders(orders);
+  // });
+})();
